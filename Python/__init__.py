4a3be867a7ba1db90473ab8d2bee603dda165d9f
Eddie Garcia <edd1e234g@gmail.com>
2020-01-14 11:15:18 -0700

    Second Commit

--- Python/__init__.py
+++
@@ -1,7 +0,0 @@
-# Created by Eddie Garcia at 1/14/20
-
-#feature name here
-# Enter feature description here
-
-# Enter scenario name here
-# Enter steps here
---
+++ Python/main.py
@@ -0,0 +1,10 @@
+# Created by Eddie Garcia at 1/14/20
+
+# Main File
+
+
+def main():
+    print("Hello World")
+
+
+main()
